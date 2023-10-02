de4b2b0bc6f71c4f4bdb0890f5d99bc87034e04a
EMINEM <hasan.qpolar@gmail.com>
2023-10-02 11:40:59 +0330

    من یه کپی از دیتا درست کردم تا ازش استفاده کنیم بعد تمام جدول هارو فراخوانی کردم و از هرکدوم یه ویوبگ درست کردم

--- testgit-on-visualstudio/Controllers/DefaultController.cs
+++ testgit-on-visualstudio/Controllers/DefaultController.cs
@@ -12,7 +12,24 @@ namespace testgit_on_visualstudio.Controllers
         // GET: Default
         public ActionResult Index()
         {
+            htdata context = new htdata();
+
+            var docs = context.tbl_doctors.Count();
+            var dep = context.tbl_skills.Count();
+            var research = context.tbl_Research.Count();
+            var awards = context.tbl_awards.Count();
+
+            ViewBag.doc = docs;
+            ViewBag.dep = dep;
+            ViewBag.research = research;
+            ViewBag.awards = awards;
+
+
+
             return View();
         }
+
+
+
     }
 }
