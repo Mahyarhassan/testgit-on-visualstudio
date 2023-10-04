e0fe065f6f76ab2bb01ebb36e593ccd23fffa409
EMINEM <hasan.qpolar@gmail.com>
2023-10-04 10:20:55 +0330

    edit js file and controler

--- testgit-on-visualstudio/Controllers/DefaultController.cs
+++ testgit-on-visualstudio/Controllers/DefaultController.cs
@@ -39,10 +39,10 @@ namespace testgit_on_visualstudio.Controllers
             return Json(dep, JsonRequestBehavior.AllowGet);
 
         }
-        public ActionResult getdoc()
+        public ActionResult getdoc(dep)
         {
 
-            var doc = context.tbl_doctors.Select(x => new { x.pkID, x.Name , x.Family }).ToList();
+            var doc = context.tbl_doctors.Where(x => x.fkskills = dep).Select(x => new { x.pkID, x.Name , x.Family }).ToList();
             return Json(doc, JsonRequestBehavior.AllowGet);
 
         }
--- testgit-on-visualstudio/assets/js/m2.js
+++ testgit-on-visualstudio/assets/js/m2.js
@@ -10,7 +10,7 @@
             for (var item in res) { 
             $("#department").append(
 
-                "<option>" + res[item].pkID + res[item].skills + "</option>"
+                "<option>" + res[item].pkID + "-" + res[item].skills + "</option>"
 
             )}
            
@@ -24,11 +24,23 @@
         });
 
 
-    $.post("/Default/getdoc")
-    
+    $("#department").change(function (event) {
+        /* alert("You have Selected  :: " + $(this).val());*/
+        var dep = $('#department').val();
+        var dep2 = dep.split("-")
+
+        var e = dep2[0];
+        alert(e);
+
+
+        $.post("/Default/getdoc", e)
+
 
         .done(function (res) {
             console.log(res)
+
+
+            
 
             //for (var item in res) {
             //    $("#doctor").append(
@@ -45,6 +57,30 @@
         .always(function () {
 
         })
+
+        //$.ajax({
+        //    type: "POST",
+        //    url: "/person/create",
+        //    dataType: "json",
+        //    contentType: "application/json; charset=utf-8",
+        //    data: jsonData,
+        //    success: function (result) {
+        //        console.log(result); //log to the console to see whether it worked
+        //    },
+        //    error: function (error) {
+        //        alert("There was an error posting the data to the server: " + error.responseText);
+        //    }
+        //});
+
+
+
+
+        
+    });
+    
+
+
+   
 
 
 
